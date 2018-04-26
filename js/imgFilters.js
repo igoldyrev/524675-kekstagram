d1aeb1c3d81c8916796f52806207069b95e67e27
Ilya Goldyrev <goldirev12@gmail.com>
2018-04-26 15:16:36 +0500

    Добавлен скрипт, который показывает после загрузки страницы блок с фильтрами для фотографий

---
+++ js/imgFilters.js
@@ -0,0 +1,7 @@
+'use strict';
+(function () {
+  window.onload = function () {
+    var imgFilters = document.querySelector('.img-filters');
+    imgFilters.classList.remove('img-filters--inactive');
+  };
+})();
