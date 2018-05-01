df10a3a406601b732ac0f83a2c0a194b51b1c11d
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-01 12:34:45 +0500

    Поправлена область видимости модуля

--- js/common.js
+++ js/common.js
@@ -1,34 +1,42 @@
 'use strict';
 
-window.common = (function () {
+(function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
   var DEBOUNCE_INTERVAL = 300;
   var lastTimeout;
 
-  return {
-    isEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
-        action();
-      }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action();
-      }
-    },
-    isGetRandom: function (min, max) {
-      return Math.round(Math.random() * (max - min) + min);
-    },
-    isDebounce: function (func) {
+  var imageUploadElement = document.querySelector('.img-upload__overlay');
+  var imageUploadImg = document.querySelector('.img-upload__preview img');
+  var picturesList = document.querySelector('.pictures');
 
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = window.setTimeout(func, DEBOUNCE_INTERVAL);
-    },
-    imageUploadElement: document.querySelector('.img-upload__overlay'),
-    imageUploadImg: document.querySelector('.img-upload__preview img'),
-    picturesList: document.querySelector('.pictures')
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
+  };
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  };
+  var isGetRandom = function (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+  var isDebounce = function (func) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(func, DEBOUNCE_INTERVAL);
+  };
+
+  window.common = {
+    imageUploadElement: imageUploadElement,
+    imageUploadImg: imageUploadImg,
+    picturesList: picturesList,
+    isEscEvent: isEscEvent,
+    isEnterEvent: isEnterEvent,
+    isGetRandom: isGetRandom,
+    isDebounce: isDebounce
   };
 })();
